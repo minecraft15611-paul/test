c910e81afde708f09276a3ebfa66bed1b9bf9f04
minecraft15611-Paul <minecraft15611@gmail.com>
2026-06-10 14:45:47 +0800

    feat: add interviewerName header to axios interceptor

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -10,6 +10,7 @@ api.interceptors.request.use((config) => {
     if (token) {
         config.headers.Authorization = `Bearer ${token}`
     }
+    config.headers.interviewerName = 'Paul Wang'
     return config
 })
 
